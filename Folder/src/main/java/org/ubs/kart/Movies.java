ae0eb52e0cf84bc996f7773776d38dedaf15a6ee
nishu vimal <vimalnishant43@gmail.com>
2024-09-15 19:02:49 +0100

    Modified Movies

--- Folder/src/main/java/org/ubs/kart/Movies.java
+++ Folder/src/main/java/org/ubs/kart/Movies.java
@@ -8,4 +8,54 @@ public class Movies {
 
     private Date yearReleased;
 
+    private String director;
+    private String genre;
+
+    public Movies(int id, String title, Date yearReleased, String director, String genre) {
+        this.id = id;
+        this.title = title;
+        this.yearReleased = yearReleased;
+        this.director = director;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getYearReleased() {
+        return yearReleased;
+    }
+
+    public void setYearReleased(Date yearReleased) {
+        this.yearReleased = yearReleased;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
