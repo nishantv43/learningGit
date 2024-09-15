d1513e0776452d2d6adbae2dcee1fe6bd36a2383
nishu vimal <vimalnishant43@gmail.com>
2024-09-15 14:38:43 +0100

    Modified User fiedls

--- Folder/src/main/java/org/ubs/kart/User.java
+++ Folder/src/main/java/org/ubs/kart/User.java
@@ -3,4 +3,25 @@ package org.ubs.kart;
 public class User {
     private Long Id;
     private String Name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public User(Long id, String name) {
+        Id = id;
+        Name = name;
+    }
 }
